205e9587403acadb627c915d87cb470c025b24c7
AstubhMishra <astubh@gmail.com>
2025-08-25 16:17:06 +0530

    new

--- vite-project/src/hooks/useSignup.jsx
+++ vite-project/src/hooks/useSignup.jsx
@@ -5,15 +5,20 @@ import axios from "axios";
 export default function useSignup(baseURL = "http://localhost:5000/api") {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [newUser, setNewUser] = useState(null);
 
-  const login = async (formData) => {
+  const signup = async (formData) => {
     setLoading(true);
     setError(null);
 
     try {
       const res = await axios.post(`${baseURL}/register`, {
-      email :formData.email,
-      password : formData.password,
+        name :formData.name,
+
+ email :formData.email,
+ password : formData.password,
+ phone :formData.phone,
+ batch :formData.batch,
       });
       
       // If backend returns token, store it
@@ -24,12 +29,12 @@ export default function useSignup(baseURL = "http://localhost:5000/api") {
       setNewUser(res.data.user || null);
       return res.data;
     } catch (err) {
-      setError(err.response?.data?.message || "Login failed");
+      setError(err.response?.data?.message || "Signup failed");
       return null;
     } finally {
       setLoading(false);
     }
   };
 
-  return { login, loading, error };
+  return { signup, loading, error, newUser };
 }
